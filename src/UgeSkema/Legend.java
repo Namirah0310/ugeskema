339d0287da39868ae50b1850e6c7a802cd53c1ea
lindkvst <lindkviste@gmail.com>
2025-09-22 14:16:53 +0200

    Legend02

--- src/UgeSkema/Legend.java
+++ src/UgeSkema/Legend.java
@@ -4,11 +4,30 @@ public class Legend {
     private String navn;
     private String farve;
 
-
+    //konstruktør til initialisering af objekt
     public Legend (String navn, String farve) {
         this.navn = navn;
         this.farve = farve;
     }
+
+    //Get
+    public String getNavn() {
+        return navn;
+    }
+
+    public String getFarve() {
+        return farve;
+    }
+
+    //Set
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public void setFarve(String farve) {
+        this.farve = farve;
+    }
+
 
 
 
