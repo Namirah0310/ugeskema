56b0ba64799ca9259e6219376e9ae9fafcd3df6f
lindkvst <lindkviste@gmail.com>
2025-09-22 14:30:22 +0200

    Test01

--- src/UgeSkema/KalenderTest.java
+++ src/UgeSkema/KalenderTest.java
@@ -1,4 +1,19 @@
 package UgeSkema;
 
 public class KalenderTest {
+    public static void main(String[] args) {
+        lavKalender();
+    }
+
+    public static void lavKalender() {
+
+        Legend l1 = new Legend("Erik", "blue");
+        Legend l2 = new Legend("Namirah", "red");
+        Legend l3 = new Legend("Johan", "orange");
+        Legend l4 = new Legend("David", "lilla");
+
+
+
+
+    }
 }
