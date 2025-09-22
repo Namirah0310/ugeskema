fa19dda4a7abe19030f6f145bd6b4482c21dc55b
lindkvst <lindkviste@gmail.com>
2025-09-22 14:07:07 +0200

    Legend01

--- src/UgeSkema/Legend.java
+++ src/UgeSkema/Legend.java
@@ -1,4 +1,15 @@
 package UgeSkema;
 
 public class Legend {
+    private String navn;
+    private String farve;
+
+
+    public Legend (String navn, String farve) {
+        this.navn = navn;
+        this.farve = farve;
+    }
+
+
+
 }
